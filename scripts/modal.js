0c3892d8d70273d270a6acf97d0457528ce15904
singhkamlesh <singhkk1993@gmail.com>
2020-05-17 15:01:58 +0530

    Hosted brand new site

---
+++ scripts/modal.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+    var modalText = {
+      shopping: {
+        title: 'Online Shopping Platform',
+        tag: 'E-commerce',
+        detail: 'A platform to control the activity of electronically buying or selling of products on online services.',
+        // link: 'https://anshitashukla.github.io'
+      },
+      navigation: {
+        title: 'Navigation Menu Animation',
+        tag: 'UI Effect',
+        detail: 'A personal website with UI animation effect for navigating through menus is designed for better user experience that works on all platforms.',
+        // link: 'https://anshitashukla.github.io'
+      },
+      training: {
+        title: 'Trainer Website',
+        tag: 'UI Artifacts',
+        detail: 'A trainer website that consists of many UI artifacts and integrate multiple videos.',
+        // link: 'https://anshitashukla.github.io'
+      }
+    };
+  
+    $('#gallery .button').on('click', function() {
+      fillModal(this.id);
+      $('.modal-wrap').addClass('visible');
+    });
+  
+    $('.close').on('click', function() {
+      $('.modal-wrap, #modal .button').removeClass('visible');
+    });
+  
+    $('.mask').on('click', function() {
+      $('.modal-wrap, #modal .button').removeClass('visible');
+    });
+  
+    var carousel = $('#carousel'),
+      slideWidth = 700,
+      threshold = slideWidth / 3,
+      dragStart,
+      dragEnd;
+  
+    setDimensions();
+  
+    $('#next').click(function() {
+      shiftSlide(-1);
+    });
+    $('#prev').click(function() {
+      shiftSlide(1);
+    });
+  
+    carousel.on('mousedown', function() {
+      if (carousel.hasClass('transition')) return;
+      dragStart = event.pageX;
+      $(this).on('mousemove', function() {
+        dragEnd = event.pageX;
+        $(this).css('transform', 'translateX(' + dragPos() + 'px)');
+      });
+      $(document).on('mouseup', function() {
+        if (dragPos() > threshold) {
+          return shiftSlide(1);
+        }
+        if (dragPos() < -threshold) {
+          return shiftSlide(-1);
+        }
+        shiftSlide(0);
+      });
+    });
+  
+    function setDimensions() {
+      if (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        )
+      ) {
+        slideWidth = $(window).innerWidth();
+      }
+      $('.carousel-wrap, .slide').css('width', slideWidth);
+      $('.modal').css('max-width', slideWidth);
+      $('#carousel').css('left', slideWidth * -1);
+    }
+  
+    function dragPos() {
+      return dragEnd - dragStart;
+    }
+  
+    function shiftSlide(direction) {
+      if (carousel.hasClass('transition')) return;
+      dragEnd = dragStart;
+      $(document).off('mouseup');
+      carousel.off('mousemove').addClass('transition').css('transform', 'translateX(' + direction * slideWidth + 'px)');
+      setTimeout(function() {
+        if (direction === 1) {
+          $('.slide:first').before($('.slide:last'));
+        } else if (direction === -1) {
+          $('.slide:last').after($('.slide:first'));
+        }
+        carousel.removeClass('transition');
+        carousel.css('transform', 'translateX(0px)');
+      }, 700);
+    }
+  
+    function fillModal(id) {
+      $('#modal .title').text(modalText[id].title);
+      $('#modal .detail').text(modalText[id].detail);
+      $('#modal .tag').text(modalText[id].tag);
+      if (modalText[id].link)
+        $('#modal .button').addClass('visible').parent()
+        .attr('href', modalText[id].link);
+  
+      $.each($('#modal li'), function(index, value) {
+        $(this).text(modalText[id].bullets[index]);
+      });
+      $.each($('#modal .slide'), function(index, value) {
+        $(this).css({background: "url('img/slides/" + id + '-' + index + ".png') center center/cover",backgroundSize: 'cover'});
+      });
+    }
+  });
+  
